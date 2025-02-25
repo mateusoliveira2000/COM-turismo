fb18f021a6cfcebf0866f14bc7fb99cfa17c134d
Mateus Oliveira <mateusoliveiracruz4@gmail.com>
2025-02-25 15:39:05 -0300

    Mudança no tempo de Carregamento

--- carregamento/carregamento.js
+++ carregamento/carregamento.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
     setTimeout(function() {
         document.getElementById("carreg").classList.add("hidden");
-    }, 3000); // Adiciona um atraso de 2 segundos
+    }, 2000); // Adiciona um atraso de 2 segundos
 });
