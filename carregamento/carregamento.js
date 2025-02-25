f7938a086db7d4411dc694e7c9052d8fccfeb751
Mateus Oliveira <mateusoliveiracruz4@gmail.com>
2025-02-25 12:07:33 -0300

    Add Tela de Carregamento

---
+++ carregamento/carregamento.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        document.getElementById("carreg").classList.add("hidden");
+    }, 3000); // Adiciona um atraso de 2 segundos
+});
